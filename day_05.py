f199d6351073ce11d993ca76fcf8cdedb466221f
Scott <scott@linfords.com>
2022-07-26 13:06:43 -0700

    Grid renders correctly

--- day_05.py
+++ day_05.py
@@ -3,14 +3,27 @@ import unittest
 
 class GridBoard:
     def __init__(self, size):
-        self.grid = None
+        self.grid = None  # initialized in reset_grid()
         self.size = size
+        self.reset_grid()
+
+    def __repr__(self):
+        return self.render_grid()
 
     def reset_grid(self):
-        self.grid =[[0 for _ in range(self.size)] for _ in range(self.size)]
+        self.grid = [[0 for _ in range(self.size)] for _ in range(self.size)]
 
     def render_grid(self):
-        pass
+        rval = []
+        for row in self.grid:
+            for int_value in row:
+                if not int_value:
+                    rval.append('.')
+                else:
+                    rval.append(str(int_value))
+            rval.append('\n')
+        return ''.join(rval)
+
 
 class Line:
     def __init__(self):
@@ -21,11 +34,10 @@ class TestGridBoard(unittest.TestCase):
     def test_init(self):
         gb = GridBoard(10)
         self.assertEqual(10, gb.size)
-        self.assertEqual(None, gb.grid)
+        self.assertNotEqual(None, gb.grid)
 
     def test_reset_grid(self):
         gb = GridBoard(10)
-        gb.reset_grid()
         self.assertEqual(0, gb.grid[0][0])
         self.assertEqual(0, gb.grid[9][9])
         with self.assertRaises(IndexError):
@@ -33,7 +45,13 @@ class TestGridBoard(unittest.TestCase):
         with self.assertRaises(IndexError):
             _ = gb.grid[0][10]
 
-
+    def test_render_grid(self):
+        gb = GridBoard(3)
+        self.assertEqual('...\n...\n...\n', gb.render_grid())
+        gb.grid[0][0] = 1
+        gb.grid[2][2] = 2
+        self.assertEqual('1..\n...\n..2\n', gb.render_grid())
+        print(gb)
 
 
 if __name__ == '__main__':
