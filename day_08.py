2e20a79dd7cbf9dbbfc1392b76e9f334e29d284a
Scott <scott@linfords.com>
2022-07-27 23:51:16 -0700

    A solution pilfered from the sea

--- day_08.py
+++ day_08.py
@@ -1,97 +1,35 @@
-import unittest
-from collections import defaultdict
+from itertools import permutations
 
+with open("signals_08.txt") as f:
+    data = f.readlines()
 
-class SignalEntry:
-    def __init__(self, patterns, output):
-        self.patterns = patterns
-        self.output = output
-    def __repr__(self):
-        return str(self.patterns) + str(self.output)
+d = {
+    "abcefg": 0,
+    "cf": 1,
+    "acdeg": 2,
+    "acdfg": 3,
+    "bcdf": 4,
+    "abdfg": 5,
+    "abdefg": 6,
+    "acf": 7,
+    "abcdefg": 8,
+    "abcdfg": 9,
+}
 
+part_1 = 0
+part_2 = 0
+for line in data:
+    a, b = line.split(" | ")
+    a = a.split()
+    b = b.split()
+    part_1 += sum(len(code) in {2, 3, 4, 7} for code in b)
+    for permutation in permutations("abcdefg"):
+        to = str.maketrans("abcdefg", "".join(permutation))
+        a_ = ["".join(sorted(code.translate(to))) for code in a]
+        b_ = ["".join(sorted(code.translate(to))) for code in b]
+        if all(code in d for code in a_):
+            part_2 += int("".join(str(d[code]) for code in b_))
+            break
 
-def sort_sort_list(item_list):
-    """
-
-    :type x: list
-    """
-    rval = []
-    for item in item_list:
-        rval.append(''.join(sorted(item)))
-    rval = sorted(rval)
-    return rval
-
-def load_signal_data(short=False):
-    filename = 'signals_short_08.txt' if short else 'signals_08.txt'
-    entries = []
-    with open(filename, 'r') as f:
-        for line in f:
-            pattern_text, output_text = line.strip().split(' | ')
-            patterns = sort_sort_list(pattern_text.split(' '))
-            output = sort_sort_list(output_text.split(' '))
-            entries.append(SignalEntry(patterns, output))
-    return entries
-
-def def_value():
-    return 0
-
-def render_dict_sorted(d):
-    """
-    :type d: dict
-    """
-    rval = []
-    rval.append('{')
-    for i, key in enumerate(sorted(d.keys())):
-        # print(key, d.get(key))
-        if i > 0:
-            rval.append(', ')
-        rval.append(str(key))
-        rval.append(': ')
-        rval.append(str(d.get(key)))
-    rval.append('}')
-    return ''.join(rval)
-
-def tally_output_lengths(entries):
-    d = defaultdict(def_value)
-    for entry in entries:
-        for x in entry.output:
-            d[len(x)] += 1
-        # print(entry, d.items())
-    return d
-
-def tally_pattern_lengths(entry):
-    d = defaultdict(def_value)
-    for x in entry.patterns:
-        d[len(x)] += 1
-    return d
-
-
-#    Easy numbers: 1, 4, 7, 8
-#  Unique lengths: 2, 4, 3, 7
-
-def sum_unique_length(lengths):
-    return lengths[2] + lengths[3] + lengths[4] + lengths[7]
-
-
-class TestSignalEntry(unittest.TestCase):
-    def test_load_signal_data(self):
-        entries = load_signal_data(True)
-        self.assertTrue(entries)
-        self.assertTrue(entries[0].patterns)
-        self.assertTrue(entries[0].output)
-        self.assertEqual(10, len(entries[0].patterns))
-        self.assertEqual(4, len(entries[0].output))
-        self.assertEqual(-1, entries[0].output[3].find('\n'))
-
-    def test_tally_lengths(self):
-        entries = load_signal_data(True)
-        lengths = tally_output_lengths(entries)
-        # print(f'2: {lengths[2]}, 3: {lengths[3]}, 4: {lengths[4]}, 7: {lengths[7]}   sum: {sum_unique_length(lengths)}')
-        self.assertEqual(26, sum_unique_length(lengths))
-
-    def test_tally_pattern_length(self):
-        entries = load_signal_data(False)
-        for entry in entries:
-            lengths = tally_pattern_lengths(entry)
-            # print(f'{lengths} : {entry}')
-            # print(render_dict_sorted(lengths), entry)
+print(part_1)
+print(part_2)
