b127b9d3bacb7b59d3023e1a0d3d89ef1ea5ef0e
Scott <scott@linfords.com>
2022-07-28 03:20:03 -0700

    Day 10 part A done

--- day_10.py
+++ day_10.py
@@ -1,55 +1,80 @@
 import unittest
-from collections import deque
-from queue import SimpleQueue
+from queue import SimpleQueue, LifoQueue
 
 
 def parse_line(text):
-    queue = SimpleQueue()
+    queue = LifoQueue()
     for c in text:
         match c:
             case '{' | '(' | '<' | '[':
-                queue.put(c)
+                queue.put_nowait(c)
             case '}' | ')' | '>' | ']':
-                if queue.qsize() == 0:
+                if queue.empty():
                     # c is an unmatched closing symbol
-                    return False
-                lefty = queue.get(block=False)
+                    return False, c
+                lefty = queue.get_nowait()
                 match lefty:
                     case '{':
                         if c != '}':
-                            return False
+                            return False, c
                     case '(':
                         if c != ')':
-                            return False
+                            return False, c
                     case '<':
                         if c != '>':
-                            return False
+                            return False, c
                     case '[':
                         if c != ']':
-                            return False
+                            return False, c
     # Unmatched closing symbol(s) left over
-    if queue.qsize() > 0:
-        return False
-    return True
+    if not queue.empty():
+        return False, queue.get_nowait()
+    return True, ''
 
+scoring_table = {')': 3, ']': 57, '}': 1197, '>': 25137}
 
-short = True
+short = False
 filename = 'subsystem_short.txt' if short else 'subsystem.txt'
+score = 0
 with open(filename, 'r') as f:
     for line in f:
-        parse_line(line.strip())
+        correct, c = parse_line(line.strip())
+        if not correct:
+            worth = scoring_table.get(c)
+            if worth:
+                score += worth
+print(score)
+if short:
+    assert(score == 26397)
+else:
+    'Answer ^^^'
 
 class TestParser(unittest.TestCase):
     def test_parser(self):
-        self.assertTrue(parse_line(''))
-        self.assertTrue(parse_line('()'))
-        self.assertTrue(parse_line('[]'))
-        self.assertTrue(parse_line('{}'))
-        self.assertTrue(parse_line('<>'))
-        self.assertFalse(parse_line('('))
-        self.assertFalse(parse_line(')'))
-        self.assertFalse(parse_line(']'))
-        self.assertFalse(parse_line('}'))
-        self.assertFalse(parse_line('>'))
-        self.assertTrue(parse_line('(((())))'))
-        self.assertFalse(parse_line('(((())>))'))
+        self.assertTrue(parse_line('')[0])
+        self.assertTrue(parse_line('()')[0])
+        self.assertTrue(parse_line('[]')[0])
+        self.assertTrue(parse_line('{}')[0])
+        self.assertTrue(parse_line('<>')[0])
+        self.assertFalse(parse_line('(')[0])
+        self.assertFalse(parse_line(')')[0])
+        self.assertFalse(parse_line(']')[0])
+        self.assertFalse(parse_line('}')[0])
+        self.assertFalse(parse_line('>')[0])
+        self.assertTrue(parse_line('(((())))')[0])
+        self.assertFalse(parse_line('(((())>))')[0])
+        self.assertFalse(parse_line('}((((<{[]}>))))')[0])
+        self.assertTrue(parse_line('()()')[0])
+        self.assertTrue(parse_line('(()())')[0])
+        correct, c = parse_line('>[()]')
+        self.assertEqual('>', c)
+        self.assertFalse(correct)
+        correct, c = parse_line('[()]}')
+        self.assertEqual('}', c)
+        self.assertFalse(correct)
+        correct, c = parse_line('[()]{')
+        self.assertEqual('{', c)
+        self.assertFalse(correct)
+        correct, c = parse_line('[]{}<>()')
+        self.assertEqual('', c)
+        self.assertTrue(correct)
